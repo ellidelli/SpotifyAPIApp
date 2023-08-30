aa4f36fdb1b5c97f4940dce598def9c1df168462
ellidelli <elizabeth.delea@gmail.com>
2023-08-30 14:55:37 -0400

    questions

--- src/pages/Playlists.vue
+++ src/pages/Playlists.vue
@@ -3,20 +3,29 @@
     <section class="sidebar">
       <h1>Explore</h1>
       <ul>
-        <li><a href="#">Songs</a></li>
-        <li><a href="#">Artists</a></li>
+        <li><a href="#">Top songs</a></li>
+        <li><a href="#">Top artists</a></li>
       </ul>
     </section>
     <section class="main">
-      <h1>hi</h1>
+      <h1>{{topMsg}}</h1>
+      <h2>{{ shorterMsg }}</h2>
+      <ul>
+      <li v-for="track in topTracks" :key="track.id">
+        <img :src="track.album.images[0].url" alt="Track Album" />
+        <div>
+          <h2>{{ track.name }}</h2>
+          <p>{{ track.artists[0].name }}</p>
+        </div>
+      </li>
+    </ul>
     </section>
     <section class="acc">
       <h1>Your Account</h1>
       <section class="info">
         <img v-if="profilePhoto" :src="profilePhoto" alt="Profile Photo" />
         <h2>{{ username }}</h2>
-        <h2>Followers: {{ followersCount }}</h2>
-        <button>Sign out</button>
+        <h3>Followers: {{ followersCount }}</h3>
       </section>
     </section>
   </div>
@@ -32,24 +41,40 @@ export default {
       playlists: [],
       username: '',
       profilePhoto: '',
-      followersCount: ''
+      followersCount: '',
+      topMsg: 'Lets get started',
+      shorterMsg: 'Global Top Tracks',
+      topTracks: []
     }
   },
     created() {
     const spotifyAccessToken = sessionStorage.getItem('SPOTIFY_ACCESS_TOKEN');
     if (spotifyAccessToken) {
-      return axios.get('https://api.spotify.com/v1/me', {
+      axios.get('https://api.spotify.com/v1/me', {
         headers: { Authorization: `Bearer ${spotifyAccessToken}` },
       })
       .then(response => {
         this.username = response.data.display_name;
         this.profilePhoto = response.data.images[0]?.url || '';
         this.followersCount = response.data.followers.total || 0;
-
         
+        // Fetch top tracks
+        axios.get('https://api.spotify.com/v1/me/top/tracks', {
+            headers: { Authorization: `Bearer ${spotifyAccessToken}` },
+            params: {
+              time_range: 'short_term', // or 'medium_term' or 'long_term'
+              limit: 10, // Number of tracks to fetch
+            },
+          })
+          .then(topTracksResponse => {
+            this.topTracks = topTracksResponse.data.items;
+          })
+          .catch(error => {
+            console.error('Error fetching top tracks:', error);
+          });
       })
       .catch(error => {
-        console.error("Error fetching username:", error);
+        console.error('Error fetching username:', error);
       });
     } else {
       return this.$router.push('/');
@@ -86,6 +111,9 @@ export default {
     width: 66.5vw;
   }
 
+  .main h2 {
+    padding-left: 30px;
+  }
   .acc {
     background-color: rgb(33, 33, 33);
     border-radius: 10px;
@@ -112,7 +140,6 @@ export default {
     height: 13vw;
     border-radius: 50%;
     margin-bottom: 10px;
-    image-rendering: -webkit-optimize-contrast;
   }
 
  h1 {
@@ -127,12 +154,23 @@ export default {
     text-align: left;
     color: white;
     font-family: "Circular", sans-serif;
-    font-size: 1.2vw;
+    font-size: 1.5vw;
     margin: 7px;
+    font-weight: 700;
+  }
+
+  h3 {
+    text-align: left;
+    color: #b3b3b3;
+    font-family: "Circular", sans-serif;
+    font-size: 1vw;
+    margin: 7px;
+    font-weight: 400;
   }
 
   ul {
     padding: 5px;
+    margin: 0;
   }
 
   ul li {
@@ -148,7 +186,7 @@ export default {
 
   ul li a {
     text-decoration: none;
-    color: white;
+    color: #b3b3b3;
     font-family: "Circular", sans-serif;
     font-size: 1.2vw;
     text-align: left;
@@ -159,7 +197,7 @@ export default {
     border: none;
     padding: 20px;
     text-decoration: none;
-    background-color: rgb(255, 255, 255, 0.3);
+    background-color: rgb(255, 255, 255, 0);
     color: white;
     margin-top: 20vh;
   }
