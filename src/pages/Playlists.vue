0471bd167461010cf1df9f3f4a4bc36f13c72d6b
ellidelli <elizabeth.delea@gmail.com>
2023-09-01 22:25:35 -0400

    updates

--- src/pages/Playlists.vue
+++ src/pages/Playlists.vue
@@ -14,14 +14,14 @@
         <h2>Global Top Tracks</h2>
         <ul>
           <li v-for="(track, index) in globalTopTracks" :key="track.id">
-          <h3>{{ index + 1 }}</h3>
-          <img :src="track.album.images[0].url" alt="Track Album" />
-          <div>
-            <h3>{{ track.name }}</h3>
-            <h3>{{ track.artists[0].name }}</h3>
-        </div>
-      </li>
-    </ul>
+            <h3>{{ index + 1 }}</h3>
+            <img :src="track.album.images[0].url" alt="Track Album" class="globalCardImg" />
+            <div>
+              <h3>{{ track.name }}</h3>
+              <h3>{{ track.artists[0].name }}</h3>
+            </div>
+          </li>
+        </ul>
       </div>
       <div v-else-if="activeSection === 'topTracks'">
         <h1>Your Top Tracks</h1>
@@ -38,7 +38,7 @@
               <h3>{{ track.artists[0].name }}</h3>
             </div>
           </li>
-         </ul>
+        </ul>
       </div>
       <div v-else-if="activeSection === 'topArtists'">
         <h1>Your Top Artists</h1>
@@ -60,7 +60,7 @@
       </div>
     </section>
     <section class="acc">
-      <h1>Your Account</h1>
+      <h1>Account</h1>
       <section class="info">
         <img v-if="profilePhoto" :src="profilePhoto" alt="Profile Photo" />
         <h2>{{ username }}</h2>
@@ -72,6 +72,7 @@
 
 <script>
 import axios from 'axios'
+import Vibrant from 'node-vibrant';
 
 export default {
   name: 'Data',
@@ -88,7 +89,8 @@ export default {
       timeRange: 'null',
       trackTimeTitle: 'Past 4 Weeks',
       artistTimeTitle: 'Past 4 Weeks',
-      globalTopTracks: []
+      globalTopTracks: [],
+      accBgColor: ''
     }
   },
   created() {
@@ -97,60 +99,60 @@ export default {
       axios.get('https://api.spotify.com/v1/me', {
         headers: { Authorization: `Bearer ${spotifyAccessToken}` },
       })
-      .then(response => {
-        this.username = response.data.display_name;
-        console.log(response.data.images)
-        this.profilePhoto = response.data.images[1]?.url || '';
-        this.followersCount = response.data.followers.total || 0;
-        
-        //fetch global 50
-        const playlistId = '37i9dQZEVXbMDoHDwVN2tF';
-
-        axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
-          headers: {
-            Authorization: `Bearer ${spotifyAccessToken}`,
-          },
-        })
         .then(response => {
-          this.globalTopTracks = response.data.items.map(item => item.track);
-          console.log(this.globalTopTracks);
-        })
-        .catch(error => {
-          console.error('Error fetching playlist tracks:', error);
-        });
+          this.username = response.data.display_name;
+          console.log(response.data.images)
+          this.profilePhoto = response.data.images[1]?.url || '';
+          this.followersCount = response.data.followers.total || 0;
 
-        // Fetch top tracks
-        axios.get('https://api.spotify.com/v1/me/top/tracks', {
+          //fetch global 50
+          const playlistId = '37i9dQZEVXbMDoHDwVN2tF';
+
+          axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+            headers: {
+              Authorization: `Bearer ${spotifyAccessToken}`,
+            },
+          })
+            .then(response => {
+              this.globalTopTracks = response.data.items.map(item => item.track);
+              console.log(this.globalTopTracks);
+            })
+            .catch(error => {
+              console.error('Error fetching playlist tracks:', error);
+            });
+
+          // Fetch top tracks
+          axios.get('https://api.spotify.com/v1/me/top/tracks', {
             headers: { Authorization: `Bearer ${spotifyAccessToken}` },
             params: {
               time_range: 'short_term',
               limit: 50,
             },
-        })
-        .then(topTracksResponse => {
-          this.topTracks = topTracksResponse.data.items;
-          this.rank += 1;
+          })
+            .then(topTracksResponse => {
+              this.topTracks = topTracksResponse.data.items;
+              this.rank += 1;
+            })
+            .catch(error => {
+              console.error('Error fetching top tracks:', error);
+            });
+
+          //fetch top artists
+          axios.get('https://api.spotify.com/v1/me/top/artists', {
+            headers: { Authorization: `Bearer ${spotifyAccessToken}` },
+            params: {
+              time_range: 'short_term',
+              limit: 50,
+            },
+          })
+            .then(topArtistsResponse => {
+              this.topArtists = topArtistsResponse.data.items;
+              this.rank += 1;
+            })
         })
         .catch(error => {
-          console.error('Error fetching top tracks:', error);
+          console.error('Error fetching username:', error);
         });
-      
-        //fetch top artists
-        axios.get('https://api.spotify.com/v1/me/top/artists', {
-          headers: { Authorization: `Bearer ${spotifyAccessToken}` },
-          params: {
-            time_range: 'short_term',
-            limit: 50,
-          },
-        })
-        .then(topArtistsResponse => {
-          this.topArtists = topArtistsResponse.data.items;
-          this.rank += 1;
-        })
-      })
-      .catch(error => {
-        console.error('Error fetching username:', error);
-      });
     } else {
       return this.$router.push('/');
     }
@@ -162,26 +164,26 @@ export default {
     getTopTracks(timeRange) {
       const spotifyAccessToken = sessionStorage.getItem('SPOTIFY_ACCESS_TOKEN');
       axios.get('https://api.spotify.com/v1/me/top/tracks', {
-            headers: { Authorization: `Bearer ${spotifyAccessToken}` },
-            params: {
-              time_range: timeRange,
-              limit: 50,
-            },
-          })
-          .then(topTracksResponse => {
-            this.topTracks = topTracksResponse.data.items;
-            this.rank += 1;
-            if (timeRange == 'short_term') {
-              this.trackTimeTitle = "Past 4 Weeks";
-            } else if (timeRange == 'medium_term') {
-              this.trackTimeTitle = "Past 6 Months"
-            } else if (timeRange == "long_term") {
-              this.trackTimeTitle = "All Time";
-            }
-          })
-          .catch(error => {
-            console.error('Error fetching top tracks:', error);
-          });
+        headers: { Authorization: `Bearer ${spotifyAccessToken}` },
+        params: {
+          time_range: timeRange,
+          limit: 50,
+        },
+      })
+        .then(topTracksResponse => {
+          this.topTracks = topTracksResponse.data.items;
+          this.rank += 1;
+          if (timeRange == 'short_term') {
+            this.trackTimeTitle = "Past 4 Weeks";
+          } else if (timeRange == 'medium_term') {
+            this.trackTimeTitle = "Past 6 Months"
+          } else if (timeRange == "long_term") {
+            this.trackTimeTitle = "All Time";
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching top tracks:', error);
+        });
     },
     getTopArtists(timeRange) {
       const spotifyAccessToken = sessionStorage.getItem('SPOTIFY_ACCESS_TOKEN');
@@ -192,198 +194,208 @@ export default {
           limit: 50,
         },
       })
-      .then(topArtistsResponse => {
-        this.topArtists = topArtistsResponse.data.items;
-        this.rank += 1;
-        if (timeRange == 'short_term') {
-          this.artistTimeTitle = "Past 4 Weeks";
-        } else if (timeRange == 'medium_term') {
-          this.artistTimeTitle = "Past 6 Months"
-        } else if (timeRange == "long_term") {
-          this.artistTimeTitle = "All Time";
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching top artists:', error);
-      });
+        .then(topArtistsResponse => {
+          this.topArtists = topArtistsResponse.data.items;
+          this.rank += 1;
+          if (timeRange == 'short_term') {
+            this.artistTimeTitle = "Past 4 Weeks";
+          } else if (timeRange == 'medium_term') {
+            this.artistTimeTitle = "Past 6 Months"
+          } else if (timeRange == "long_term") {
+            this.artistTimeTitle = "All Time";
+          }
+        })
+        .catch(error => {
+          console.error('Error fetching top artists:', error);
+        });
+
     }
   },
   computed: {
     trackCount() {
       return this.topTracks.map((track, index) => index + 1);
     },
-  }
-}
+  },
+};
+
 </script>
 
 <style scoped>
-  .layout {
-    display: flex;
-    flex-direction: row;
-    align-items:flex-start;
-    justify-content: space-between;
-    width: 100%;
-    height: 100vh;
-    background-color: rgb(18, 18, 18);
-    position: absolute;
-  }
+.layout {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: space-between;
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(18, 18, 18);
+  position: absolute;
+}
 
-  .sidebar {
-    background-color: rgb(33, 33, 33);
-    border-radius: 10px;
-    margin-top: 20px;
-    min-height: 95vh;
-    height: fit-content;
-    width: 13.5vw;
-    margin-left: 1vw;
-    display: flex;
-    flex-direction: column;
-  }
+.sidebar {
+  background-color: rgb(33, 33, 33);
+  border-radius: 10px;
+  margin-top: 20px;
+  min-height: 95vh;
+  height: fit-content;
+  width: 13.5vw;
+  margin-left: 1vw;
+  display: flex;
+  flex-direction: column;
+}
 
-  .main {
-    background-color: rgb(33, 33, 33);
-    border-radius: 10px;
-    margin-top: 20px;
-    height: 95vh;
-    width: 66.5vw;
-    overflow:scroll;
-  }
+.main {
+  background-color: rgb(33, 33, 33);
+  border-radius: 10px;
+  margin-top: 20px;
+  height: 95vh;
+  width: 66.5vw;
+  overflow: scroll;
+}
 
-  .main h2 {
-    padding-left: 30px;
-  }
-  .acc {
-    background-color: rgb(33, 33, 33);
-    border-radius: 10px;
-    margin-top: 20px;
-    min-height: 95vh;
-    height: fit-content;
-    width: 16.5vw;
-    margin-right: 1vw;
-  }
+.main h2 {
+  padding-left: 30px;
+}
 
-  .acc > h1 {
-    padding-left: 0;
-    text-align: center;
-  }
+.acc {
+  /* background-color: rgb(33, 33, 33); */
+  border-radius: 10px;
+  margin-top: 20px;
+  min-height: 95vh;
+  height: fit-content;
+  width: 16.5vw;
+  margin-right: 1vw;
+}
 
-  .info {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-  }
+/* .main>div>ul>li>img {
+  height: 15vh;
+  width: auto;
+} */
 
-  img {
-    width: 13vw;
-    height: 13vw;
-    border-radius: 50%;
-    margin-bottom: 10px;
-  }
+.acc>h1 {
+  padding-left: 0;
+  text-align: center;
+}
 
- h1 {
-    text-align: left;
-    color: white;
-    font-family: "Circular", sans-serif;
-    font-size: 2vw;
-    padding-left: 15px;
-  }
+.info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
 
-  h2 {
-    text-align: left;
-    color: white;
-    font-family: "Circular", sans-serif;
-    font-size: 1.5vw;
-    margin: 7px;
-    font-weight: 700;
-  }
+img {
+  width: 13vw;
+  height: 13vw;
+  border-radius: 50%;
+  /* margin-bottom: 10px; */
+}
 
-  h3 {
-    text-align: left;
-    color: #b3b3b3;
-    font-family: "Circular", sans-serif;
-    font-size: 1vw;
-    margin: 7px;
-    font-weight: 400;
-  }
+h1 {
+  text-align: left;
+  color: white;
+  font-family: "Circular", sans-serif;
+  font-size: 2vw;
+  padding-left: 15px;
+}
 
-  ul {
-    padding: 5px;
-    margin: 0;
-  }
+h2 {
+  text-align: left;
+  color: white;
+  font-family: "Circular", sans-serif;
+  font-size: 1.5vw;
+  margin: 7px;
+  font-weight: 700;
+}
 
-  ul li {
-    list-style: none;
-    padding: 10px;
-    padding-left: 15px;
-  }
+h3 {
+  text-align: left;
+  color: #b3b3b3;
+  font-family: "Circular", sans-serif;
+  font-size: 1vw;
+  margin: 7px;
+  font-weight: 400;
+}
 
-  ul li:hover {
-    background-color: #535353;
-    border-radius: 10px;
-  }
+ul {
+  padding: 5px;
+  margin: 0;
+}
 
-  ul li a {
-    text-decoration: none;
-    color: #b3b3b3;
-    font-family: "Circular", sans-serif;
-    font-size: 1.2vw;
-    text-align: left;
-  }
+ul li {
+  list-style: none;
+  padding: 10px;
+  padding-left: 15px;
+}
 
-  button {
-    border-radius: 10px;
-    border: none;
-    padding: 20px;
-    text-decoration: none;
-    background-color: rgb(255, 255, 255, 0);
-    color: white;
-    margin-top: 20vh;
-  }
+ul li:hover {
+  background-color: #535353;
+  border-radius: 10px;
+}
 
-  button:hover {
-    background-color: rgb(255, 255, 255, 0.7);
-  }
+ul li a {
+  text-decoration: none;
+  color: #b3b3b3;
+  font-family: "Circular", sans-serif;
+  font-size: 1.2vw;
+  text-align: left;
+}
 
-  .main > div > ul > li {
-    padding-left: 30px;
-    display: flex;
-    flex-direction: row;
-  }
+button {
+  border-radius: 10px;
+  border: none;
+  padding: 20px;
+  text-decoration: none;
+  background-color: rgb(255, 255, 255, 0);
+  color: white;
+  margin-top: 20vh;
+}
 
-  .main > div > ul > li > img {
-    max-height: 5vh;
-    width: auto;
-    border-radius: 5px;
-  }
+button:hover {
+  background-color: rgb(255, 255, 255, 0.7);
+}
 
-  .sidebar > ul >li > button {
-    margin: 0;
-    padding: 0;
-    font-size: 1.2vw;
-    color: #b3b3b3;
-  }
+.main>div>ul>li {
+  padding-left: 30px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
 
-  .sidebar > ul >li > button:hover {
-    background-color: #535353;
-    border-radius: 10px;
-  }
-  .main > div > button {
-    margin: 0;
-    margin-left: 30px;
-    font-size: 1vw;
-    padding: 1vh 2vw;
-    background-color: #1db954;
-    border-radius: 5px;
-  }
-  .main > div > button:hover {
-    margin: 0;
-    margin-left: 30px;
-    font-size: 1vw;
-    padding: 1vh 2vw;
-    background-color: rgb(29,185,84, 0.5);
-    border-radius: 5px;
-  }
+.main>div>ul>li>img {
+  max-height: 5vh;
+  width: auto;
+  border-radius: 5px;
+}
 
+.sidebar>ul>li>button {
+  margin: 0;
+  padding: 0;
+  font-size: 1.2vw;
+  color: #b3b3b3;
+}
+
+.sidebar>ul>li>button:hover {
+  background-color: #535353;
+  border-radius: 10px;
+}
+
+.main>div>button {
+  margin: 0;
+  margin-left: 30px;
+  font-size: 1vw;
+  padding: 1vh 2vw;
+  background-color: #1db954;
+  border-radius: 5px;
+}
+
+.main>div>button:hover {
+  margin: 0;
+  margin-left: 30px;
+  font-size: 1vw;
+  padding: 1vh 2vw;
+  background-color: rgb(29, 185, 84, 0.5);
+  border-radius: 5px;
+}
 </style>
